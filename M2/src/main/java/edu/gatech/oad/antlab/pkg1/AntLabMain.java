4040a94d7d47f6929c8acd163c32fe5f9b68cc85
Walker Powell <walkerhpowell@gmail.com>
2017-01-28 12:38:14 -0500

    Updated AntLabMain.java with P3 info

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,16 +65,16 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("bgeier6");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Gabriel Nakajima An");
-                  toPrint += p5.toString("gan9");
+		  Person3 p3 = new Person3("Walker Powell");
+		  toPrint += p3.toString("wpowell9");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("P4");
+          toPrint += p4.toString("gburdell4");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("Gabriel Nakajima An");
+          toPrint += p5.toString("gan9");
 		  
         System.out.println(toPrint);
         
